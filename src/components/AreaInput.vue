b47ebdef567cff45a0b8963a955b448a6a39835b
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-06-29 21:07:39 -0300

    feat: create form component to area input

---
+++ src/components/AreaInput.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div class="w-full">
+        <label 
+            for=""
+            class="text-gray-200">
+                {{ label }}
+        </label>
+
+        <textarea
+            class="w-full min-h-[200px] rounded-lg mt-3 border border-[#3f3f46] p-4 focus:outline-none focus:ring focus:ring-[#34d399]"></textarea>
+    </div>
+
+
+</template>
+
+
+<script setup lang="ts">
+
+
+
+defineProps<{
+    label: string
+}>()
+
+</script>
