f7cab0b1f91c3e1914b125867f1e1ddb4d6df5df
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-06-29 22:11:35 -0300

    refact: add error message

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -2,25 +2,53 @@
 
     <div class="w-full">
         <label 
-            for=""
+            :for="id"
             class="text-gray-200">
                 {{ label }}
         </label>
 
         <input
+            :id="id"
             :type="type"
-            class="w-full h-10 rounded-lg my-3 border border-[#3f3f46] px-4 focus:outline-none focus:ring focus:ring-[#34d399] bg-[#09090b]"
+            class="w-full h-10 rounded-lg my-3 border border-[#3f3f46] px-4 focus:outline-none focus:ring focus:ring-[#34d399] bg-[#09090b]" />
+
+        <small 
+            v-if="hasError" 
+            class="text-red-600 mt-1">
+                {{ error }}
+        </small>
     </div>
 
 </template>
 
 <script setup lang="ts">
 
+import { computed } from 'vue'
 
-defineProps<{
-    label: string,
-    type: 'text' | 'number',
-}>()
 
+const props = defineProps({
+    id: {
+        type: String,
+        required: true,
+    },
+
+    label: {
+        type: String,
+        required: false,
+    },
+
+    type: {
+        type: String,
+        required: true,
+    },
+
+    error: {
+        type: String,
+        required: false,
+    }
+})
+
+
+const hasError = computed(() => props.error && props.error.length)
 
 </script>
