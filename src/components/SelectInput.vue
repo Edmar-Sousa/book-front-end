71772b034c6afef93b9f0d2c9acdd959860e3434
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-06-29 22:11:22 -0300

    refact: add error message

--- src/components/SelectInput.vue
+++ src/components/SelectInput.vue
@@ -14,6 +14,12 @@
             :id="id"
             class="w-full h-10 rounded-lg mt-3 border border-[#3f3f46] px-4 focus:outline-none focus:ring focus:ring-[#34d399]">
         </multi-select>
+
+        <small 
+            v-if="hasError" 
+            class="text-red-600 mt-1">
+                {{ error }}
+        </small>
     </div>
 
 </template>
@@ -21,17 +27,36 @@
 
 <script setup lang="ts">
 
+import { computed } from 'vue'
 import MultiSelect from 'primevue/multiselect'
 
 
-defineProps<{
-    id: string,
-    options: Array<Object>,
-    label: string,
-}>()
+const props = defineProps({
+    id: {
+        type: String,
+        required: true,
+    },
+
+    label: {
+        type: String,
+        required: false,
+    },
+
+    options: {
+        type: Array<Object>,
+        required: true,
+    },
+
+    error: {
+        type: String,
+        required: false,
+    }
+})
 
 
 const modal = defineModel()
 
+
+const hasError = computed(() => props.error && props.error.length)
 
 </script>
