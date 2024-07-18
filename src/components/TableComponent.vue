0944a8ecb635ec79b24f3b8eb0bf1ab4f311d91e
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-07-17 22:44:34 -0300

    feat: create table component with schelenton

---
+++ src/components/TableComponent.vue
@@ -0,0 +1,81 @@
+<template>
+
+    <template v-if="isLoading">
+        <data-table 
+            :value="loaddingSkeletonTable"
+            striped-rows
+            paginator
+            :rows="5"
+            :rows-per-page-options="[5, 10, 20, 50]">
+                <column 
+                    v-for="(column, index) in columns"
+                    :key="index"
+                    :field="column.field" 
+                    :header="column.header">
+                        <template #body>
+                            <skeleton />
+                        </template>
+                </column>
+        </data-table>
+    </template>
+
+    <template v-else>
+        <data-table 
+            :value="data"
+            striped-rows
+            paginator
+            :rows="rows"
+            :rows-per-page-options="paginatorPerPages">
+                <column 
+                    v-for="(column, index) in columns"
+                    :key="index"
+                    :field="column.field" 
+                    :header="column.header" />
+        </data-table>
+    </template>
+
+</template>
+
+<script setup lang="ts">
+
+
+import { ColumnType } from '@/interfaces/TableColumnType'
+
+import Skeleton from 'primevue/skeleton'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+
+
+defineProps({
+    isLoading: {
+        type: Boolean,
+        default: false,
+        required: false,
+    },
+
+    paginatorPerPages: {
+        type: Array<Number>,
+        default: [5, 10, 20, 50],
+        required: false,
+    },
+
+    rows: {
+        type: Number,
+        default: 5,
+        required: false,
+    },
+
+    columns: {
+        type: Array<ColumnType>,
+        required: true,
+    },
+
+    data: {
+        type: Array<any>,
+    },
+})
+
+
+const loaddingSkeletonTable = new Array(4)
+
+</script>
---
+++ src/interfaces/TableColumnType.ts
@@ -0,0 +1,4 @@
+export interface ColumnType {
+    header: string
+    field: string
+}
